71d2c04b2422e6c41f8fb716dadf1107a5c77f83
thaops <nguyenpham0666@gmail.com>
2024-04-11 14:51:31 +0700

    api sever

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect (){
 try {
-    await mongoose.connect('mongodb://localhost:27017/ASM02');
+    await mongoose.connect('mongodb+srv://thaops:0933243805Asus@appshop.rasnfgy.mongodb.net/ASM02');
     console.log("ket noi thanh cong")
 } catch (error) {
     console.log("ket noi khong thanh cong")
